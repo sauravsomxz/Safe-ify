d8195075abfa6e3519a61e3bd9034624753c4157
pulkit-2025 <2025pulkit@gmail.com>
2021-05-09 12:56:17 +0530

    Added route variable for add_mates

--- safeify/lib/main.dart
+++ safeify/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:safeify/views/add_mates.dart';
 import 'package:safeify/views/map.dart';
 import 'package:safeify/views/welcome_screen.dart';
 import 'package:safeify/views/login_screen.dart';
@@ -21,6 +22,7 @@ class MyApp extends StatelessWidget {
         RegistrationScreen.id : (context) => RegistrationScreen(),
         WelcomeScreen.id : (context) => WelcomeScreen(),
         HomePage.id : (context) => HomePage(),
+        AddMates.id : (context) => AddMates(),
       },
       home: WelcomeScreen(),
       debugShowCheckedModeBanner: false,
